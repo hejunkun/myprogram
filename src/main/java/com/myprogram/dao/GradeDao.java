886a37e3225f1fe0a5a2bce10ba66961265e6d6f
hjk <2021996351@qq.com>
2018-01-22 23:35:48 +0800

    提交myprogram

---
+++ src/main/java/com/myprogram/dao/GradeDao.java
@@ -0,0 +1,10 @@
+package com.myprogram.dao;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by hjk on 2018/1/8.
+ */
+@Repository("gradeDao")
+public class GradeDao extends DaoUtil {
+}
